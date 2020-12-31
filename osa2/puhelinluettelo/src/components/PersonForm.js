1155de6cfd91ba4fcc7800f7a5a31581a36f40de
irtep <rasanen.petri@gmail.com>
2020-12-31 13:33:15 +0200

    2.18 ready

--- osa2/puhelinluettelo/src/components/PersonForm.js
+++ osa2/puhelinluettelo/src/components/PersonForm.js
@@ -4,8 +4,8 @@ const PersonForm = ({addNew, handleNameChange, handleNumberChange}) => {
   return (
     <>
     <form onSubmit= {addNew}>
-      <div>name: <input onChange= {handleNameChange}/></div>
-      <div>number: <input type= "number" onChange= {handleNumberChange}/></div>
+      <div>name: <input id= "nameField" onChange= {handleNameChange}/></div>
+      <div>number: <input id= "numberField" type= "number" onChange= {handleNumberChange}/></div>
       <div>
         <button type="submit">add</button>
       </div>
--- osa2/puhelinluettelo/src/components/ShowPersons.js
+++ osa2/puhelinluettelo/src/components/ShowPersons.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import Person from './Person';
+import ActionButton from './ActionButton';
 
-const ShowPersons = ({showThese}) => {
+const ShowPersons = ({showThese, deleteFunc}) => {
   return (
     <>
     {showThese.map( person => {
       return(
-        <Person
-        key= {person.name}
-        person= {person}
-        />
+        <div key= {person.name}>
+          <Person
+            person= {person}
+          />
+          <ActionButton
+            id= {person.id}
+            action= {deleteFunc}
+            name= {person.name}
+          />
+        </div>
        )
     })}
     </>
