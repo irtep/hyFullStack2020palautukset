e3ed54c3405a0f10491fac938c6728c1c10a1615
irtep <rasanen.petri@gmail.com>
2021-01-10 00:27:02 +0200

    stuck with test: 4.11, but contiuing from this

--- osa4/blogilista/controllers/blogs.js
+++ osa4/blogilista/controllers/blogs.js
@@ -18,5 +18,17 @@ blogsRouter.post('/', (request, response) => {
       response.status(201).json(result);
     });
 });
+// show a certain that hits with id param given
+blogsRouter.get('/:id', (req, res) => {
+  const id = req.params.id;
+  Blog.findById(id).then(note => {
+    if (note) {
+      res.json(note);
+    } else {
+      res.status(404).end();
+    }
+  });
+//    .catch(error => next(error));
+});
 
 module.exports = blogsRouter;
