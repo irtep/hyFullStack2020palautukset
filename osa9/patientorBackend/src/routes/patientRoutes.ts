5bf97abb7ce816f3e242e707c1a063b51d4174bb
irtep <rasanen.petri@gmail.com>
2021-02-16 21:44:49 +0200

    9.11 done

--- osa9/patientorBackend/src/routes/patientRoutes.ts
+++ osa9/patientorBackend/src/routes/patientRoutes.ts
@@ -6,10 +6,10 @@ router.get('/', (_req, res) => {
   console.log('requesting patients');
   res.send(patientServices.getEntries());
 });
-
+/*
 router.post('/', (req, res) => {
   const adding = patientServices.addEntry(req.body);
   res.json(adding);
 });
-
+*/
 export default router;
